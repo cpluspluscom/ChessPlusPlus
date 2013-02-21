180a177c97fd6f89840111b4c85950dbb07a903b
Thumperrr <sbhall808@hotmail.com>
2013-02-21 11:29:10 -0500

    Created some core framework for the flow of the game.
    Descriptions:
    class SFMLEvent is designed to be an interface to every event possible in SFML. It takes an sf::Event object, and calls certain functions depending on what that event is. For example, sf::Event contains a left click at (100, 250). SFLMEvent calls the virtual function OnLButtonPressed(100, 250). These virtual functions are reimplemented by base classes to handle events the way they want to.
    
    class Application is the core component of the game. It has ownership of the sf::RenderWindow object, which is passed as a pointer to all other objects that need to draw something. It contains the event loop (polls for events from display) and forwards these events to the appropriate app state. The entire framework is basically broken down into 3 functions:
    *onEvent(sf::Event *Event);
    *onLoop();
    *onRender();
    
    class AppStateManager centralizes application states. Application calls corresponding AppStateManager functions, and AppStateManager sends that call to the current state. (i.e. in Application::onEvent, calls AppStateManager::onEvent, which calls currentState::onEvent). Everything basically trickles down until it gets where it needs to go.
    
    AppStateGame is a placeholder class intended to eventually be where all the game logic takes place. At the moment all it is doing is providing an outline and working as a proof of concept (there are some example event functions and cout calls).

---
+++ src/AppState.h
@@ -0,0 +1,19 @@
+#ifndef _APPSTATE_H
+#define _APPSTATE_H
+#include "SFMLEvent.h"
+
+//pure virtual abstract base class for game state management. 
+class AppState : public SFMLEvent
+{
+public:
+	AppState() {}
+	virtual ~AppState() {}
+
+	virtual void onActivate() = 0;
+	virtual void onEvent(sf::Event *Event) = 0;
+	virtual void onLoop() = 0;
+	virtual void onRender(sf::RenderWindow *display) = 0;
+	virtual void onDeactivate() = 0;
+};
+
+#endif
---
+++ src/SFMLEvent.cpp
@@ -0,0 +1,202 @@
+#include "SFMLEvent.h"
+
+SFMLEvent::SFMLEvent()
+{
+}
+
+SFMLEvent::~SFMLEvent()
+{
+}
+
+void SFMLEvent::OnEvent(sf::Event *Event)
+{
+	switch(Event->type)
+	{
+	case sf::Event::Closed:
+		{
+			OnClosed();
+			break;
+		}
+	case sf::Event::Resized:
+		{
+			OnResized(Event->size.width, Event->size.height);
+			break;
+		}
+	case sf::Event::LostFocus:
+		{
+			OnLostFocus();
+			break;
+		}
+	case sf::Event::GainedFocus:
+		{
+			OnGainedFocus();
+			break;
+		}
+	case sf::Event::TextEntered:
+		{
+			OnTextEntered(Event->text.unicode);
+			break;
+		}
+	case sf::Event::KeyPressed:
+		{
+			OnKeyPressed(Event->key.code, Event->key.alt, Event->key.control, Event->key.shift, Event->key.system);
+			break;
+		}
+	case sf::Event::KeyReleased:
+		{
+			OnKeyReleased(Event->key.code, Event->key.alt, Event->key.control, Event->key.shift, Event->key.system);
+			break;
+		}
+	case sf::Event::MouseWheelMoved:
+		{
+			OnMouseWheelMoved(Event->mouseWheel.delta, Event->mouseWheel.x, Event->mouseWheel.y);
+			break;
+		}
+	case sf::Event::MouseButtonPressed:
+		{
+			switch(Event->mouseButton.button)
+			{
+			case sf::Mouse::Left:
+				{
+					OnLButtonPressed(Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			case sf::Mouse::Right:
+				{
+					OnRButtonPressed(Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			case sf::Mouse::Middle:
+				{
+					OnMButtonPressed(Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			default:
+				{
+					OnMouseButtonPressed(Event->mouseButton.button, Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			}
+			break;
+		}
+	case sf::Event::MouseButtonReleased:
+		{
+			switch(Event->mouseButton.button)
+			{
+			case sf::Mouse::Left:
+				{
+					OnLButtonReleased(Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			case sf::Mouse::Right:
+				{
+					OnRButtonReleased(Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			case sf::Mouse::Middle:
+				{
+					OnMButtonReleased(Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			default:
+				{
+					OnMouseButtonPressed(Event->mouseButton.button, Event->mouseButton.x, Event->mouseButton.y);
+					break;
+				}
+			}
+			break;
+		}
+	case sf::Event::MouseMoved:
+		{
+			OnMouseMoved(Event->mouseMove.x, Event->mouseMove.y);
+			break;
+		}
+	case sf::Event::MouseEntered:
+		{
+			OnMouseEnteredWindow();
+			break;
+		}
+	case sf::Event::MouseLeft:
+		{
+			OnMouseLeftWindow();
+			break;
+		}
+	case sf::Event::JoystickButtonPressed:
+		{
+			OnJoystickButtonPressed(Event->joystickButton.joystickId, Event->joystickButton.button);
+			break;
+		}
+	case sf::Event::JoystickButtonReleased:
+		{
+			OnJoystickButtonReleased(Event->joystickButton.joystickId, Event->joystickButton.button);
+			break;
+		}
+	case sf::Event::JoystickMoved:
+		{
+			OnJoystickMoved(Event->joystickMove.joystickId, Event->joystickMove.axis, Event->joystickMove.position);
+			break;
+		}
+	case sf::Event::JoystickConnected:
+		{
+			OnJoystickConnected(Event->joystickConnect.joystickId);
+			break;
+		}
+	case sf::Event::JoystickDisconnected:
+		{
+			OnJoystickDisconnected(Event->joystickConnect.joystickId);
+			break;
+		}
+	default:
+			break;
+	}
+}
+
+//pure virtual, do nothing
+void SFMLEvent::OnClosed() {
+}
+void SFMLEvent::OnResized(uint w, uint h) {
+}
+void SFMLEvent::OnLostFocus() {
+}
+void SFMLEvent::OnGainedFocus() {
+}
+void SFMLEvent::OnTextEntered(UINT32 unicode) {
+}
+void SFMLEvent::OnKeyPressed(sf::Keyboard::Key key, bool alt, bool control, bool shift, bool system) {
+}
+void SFMLEvent::OnKeyReleased(sf::Keyboard::Key key, bool alt, bool control, bool shift, bool system) {
+}
+void SFMLEvent::OnMouseWheelMoved(int delta, int x, int y) {
+}
+void SFMLEvent::OnLButtonPressed(int x, int y) {
+}
+void SFMLEvent::OnLButtonReleased(int x, int y) {
+}
+void SFMLEvent::OnRButtonPressed(int x, int y) {
+}
+void SFMLEvent::OnRButtonReleased(int x, int y) {
+}
+void SFMLEvent::OnMButtonPressed(int x, int y) {
+}
+void SFMLEvent::OnMButtonReleased(int x, int y) {
+}
+void SFMLEvent::OnMouseButtonPressed(sf::Mouse::Button button, int x, int y) {
+}
+void SFMLEvent::OnMouseButtonReleased(sf::Mouse::Button button, int x, int y) {
+}
+void SFMLEvent::OnMouseMoved(int x, int y) {
+}
+void SFMLEvent::OnMouseEnteredWindow() {
+}
+void SFMLEvent::OnMouseLeftWindow() {
+}
+void SFMLEvent::OnJoystickButtonPressed(uint joystickID, uint button) {
+}
+void SFMLEvent::OnJoystickButtonReleased(uint joystickID, uint button) {
+}
+void SFMLEvent::OnJoystickMoved(uint joystickID, sf::Joystick::Axis axis, float position) {
+}
+void SFMLEvent::OnJoystickConnected(uint joystickID) {
+}
+ void SFMLEvent::OnJoystickDisconnected(uint joystickID) {
+}

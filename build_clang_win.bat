e40ca2e04a9fbfd853ab8834b4d22f149c9d899a
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-04-23 20:48:29 -0500

    Work on clang_win batch

--- build_clang_win.bat
+++ build_clang_win.bat
@@ -1,2 +1,2 @@
-clang++ -std=c++11 -o chesspp.exe -isystem lib/boost/boost/ -isystem lib/SFML/include/ -DSFML_STATIC -isystem lib/json-parser/ -Llib/SFML/lib/ -I src/ lib/json-parser/json.c src/*.cpp src/chesspp/*.cpp src/chesspp/board/*.cpp src/chesspp/graphics/*.cpp src/chesspp/log/*.cpp -lsfml-system-s -lsfml-graphics -lsfml-window-s
+clang++ -std=c++11 -o chesspp.exe -isystem lib/boost/boost/ -isystem lib/SFML/include/ -isystem lib/json-parser/ -Llib/SFML/lib/ -I src/ lib/json-parser/json.c src/*.cpp src/chesspp/*.cpp src/chesspp/board/*.cpp src/chesspp/graphics/*.cpp src/chesspp/log/*.cpp -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
 pause

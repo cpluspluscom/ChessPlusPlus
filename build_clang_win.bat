b1237b7aa939d342ac0cf2a1f28ae0ca13413cce
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-04-24 16:17:00 -0500

    Re-fix problems I don't know how or why but somehow these fixes were reverted.

--- build_clang_win.bat
+++ build_clang_win.bat
@@ -1,2 +1,2 @@
-clang++ -std=c++11 -o chesspp.exe -isystem lib/boost/boost/ -isystem lib/SFML/include/ -isystem lib/json-parser/ -Llib/SFML/lib/ -I src/ lib/json-parser/json.c src/*.cpp src/chesspp/*.cpp src/chesspp/board/*.cpp src/chesspp/graphics/*.cpp src/chesspp/log/*.cpp -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
+clang++ -std=c++11 -o chesspp.exe -isystem lib/boost/boost/ -isystem lib/SFML/include/ -isystem lib/json-parser/ -Llib/SFML/lib/ -I src/ lib/json-parser/json.c src/*.cpp src/board/*.cpp src/graphics/*.cpp src/log/*.cpp -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
 pause

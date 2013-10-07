68d66f4674571ef13858b284b0eeeebda6955ccf
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-10-07 14:28:56 -0500

    Use an inline namespace to be proper

--- src/util/Utilities.hpp
+++ src/util/Utilities.hpp
@@ -5,50 +5,53 @@
 
 namespace chesspp
 {
-    /**
-	 * Alternative global operator!=.
-	 * Smarter template than the one in ::std::rel_ops,
-	 * supports inheritance and non-bool return type.
-	 * \tparam T the left-hand type.
-	 * \tparam U the right-hand type.
-	 * \param t the left-hand value.
-	 * \param u the right-hand value.
-	 * \return !(t == u)
-	 */
-    template<typename T, typename U>
-    auto operator!=(T const &t, U const &u) noexcept(noexcept(!(t == u)))
-    -> typename std::enable_if<std::is_base_of<T, U>::value || std::is_base_of<U, T>::value, decltype(!(t == u))>::type
+    inline namespace util
     {
-        return !(t == u);
-    }
-
-    /**
-     * Better version of std::make_signed that supports
-     * floating point types, usage is identical.
-     * \tparam T must be scalar.
-     */
-    template<typename T>
-    struct MakeSigned
-    {
-        static_assert(std::is_scalar<T>::value, "Template parameter must be a scalar type");
         /**
-         * The signed variant of T, or T if T is floating point.
+         * Alternative global operator!=.
+         * Smarter template than the one in ::std::rel_ops,
+         * supports inheritance and non-bool return type.
+         * \tparam T the left-hand type.
+         * \tparam U the right-hand type.
+         * \param t the left-hand value.
+         * \param u the right-hand value.
+         * \return !(t == u)
          */
-        using type =
-            typename std::conditional
-            <
-                std::is_floating_point<T>::value,
-                std::common_type<T>,
-                std::make_signed<T>
-            >::type::type;
-    };
-    /**
-     * Template alias to complement MakeSigned.
-     * Used the same way as std::is_signed.
-     * \tparam T must be scalar.
-     */
-    template<typename T>
-    using IsSigned = typename std::is_same<T, typename MakeSigned<T>::type>/*::value*/;
+        template<typename T, typename U>
+        auto operator!=(T const &t, U const &u) noexcept(noexcept(!(t == u)))
+        -> typename std::enable_if<std::is_base_of<T, U>::value || std::is_base_of<U, T>::value, decltype(!(t == u))>::type
+        {
+            return !(t == u);
+        }
+
+        /**
+         * Better version of std::make_signed that supports
+         * floating point types, usage is identical.
+         * \tparam T must be scalar.
+         */
+        template<typename T>
+        struct MakeSigned
+        {
+            static_assert(std::is_scalar<T>::value, "Template parameter must be a scalar type");
+            /**
+             * The signed variant of T, or T if T is floating point.
+             */
+            using type =
+                typename std::conditional
+                <
+                    std::is_floating_point<T>::value,
+                    std::common_type<T>,
+                    std::make_signed<T>
+                >::type::type;
+        };
+        /**
+         * Template alias to complement MakeSigned.
+         * Used the same way as std::is_signed.
+         * \tparam T must be scalar.
+         */
+        template<typename T>
+        using IsSigned = typename std::is_same<T, typename MakeSigned<T>::type>/*::value*/;
+    }
 }
 
 #endif

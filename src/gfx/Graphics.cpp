0fa96a0af92185468186efadba01e4ee1bddc0f4
Stephen Hall <sbhall808@hotmail.com>
2013-12-27 16:14:16 -0500

    Resource management refactor. There now exists a GenericResourceManager class that can be declared with different template parameters to manage different types of resources.

--- src/gfx/Graphics.cpp
+++ src/gfx/Graphics.cpp
@@ -1,6 +1,6 @@
 #include "Graphics.hpp"
 
-#include "TextureManager.hpp"
+#include "ResourceManager.hpp"
 #include "config/Configuration.hpp"
 
 #include <iostream>
@@ -13,11 +13,11 @@ namespace chesspp
         : display(disp)
         , gfx_config(gfxc)
         , board_config(bc)
-        , board        (TextureManager::instance().load(gfx_config.spritePath("board", "board"        )))
-        , valid_move   (TextureManager::instance().load(gfx_config.spritePath("board", "valid move"   )))
-        , enemy_move   (TextureManager::instance().load(gfx_config.spritePath("board", "enemy move"   )))
-        , valid_capture(TextureManager::instance().load(gfx_config.spritePath("board", "valid capture")))
-        , enemy_capture(TextureManager::instance().load(gfx_config.spritePath("board", "enemy capture")))
+        , board        (TextureManager.load(gfx_config.spritePath("board", "board"        )))
+        , valid_move   (TextureManager.load(gfx_config.spritePath("board", "valid move"   )))
+        , enemy_move   (TextureManager.load(gfx_config.spritePath("board", "enemy move"   )))
+        , valid_capture(TextureManager.load(gfx_config.spritePath("board", "valid capture")))
+        , enemy_capture(TextureManager.load(gfx_config.spritePath("board", "enemy capture")))
         {
         }
 
@@ -32,12 +32,12 @@ namespace chesspp
         }
         void GraphicsHandler::drawPiece(board::Piece const &p)
         {
-            sf::Sprite piece {TextureManager::instance().load(p.texture())};
+            sf::Sprite piece {TextureManager.load(p.texture())};
             drawSpriteAtCell(piece, p.pos.x, p.pos.y);
         }
         void GraphicsHandler::drawPieceAt(board::Piece const &p, sf::Vector2i const &pos)
         {
-            sf::Sprite piece {TextureManager::instance().load(p.texture())};
+            sf::Sprite piece {TextureManager.load(p.texture())};
             piece.setPosition(pos.x - (board_config.cellWidth()/2), pos.y - (board_config.cellHeight()/2));
             display.draw(piece);
         }

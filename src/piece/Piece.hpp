6772a5360e532735b6c40bd5f70e030550d75d18
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-02-23 15:12:08 -0600

    Document piece/Piece.hpp

--- src/piece/Piece.hpp
+++ src/piece/Piece.hpp
@@ -16,6 +16,10 @@ namespace chesspp
     }
     namespace piece
     {
+        /**
+         * \brief
+         * A chess piece.
+         */
         class Piece
         {
             friend class ::chesspp::board::Board;
@@ -25,6 +29,10 @@ namespace chesspp
             using Suit_t     = config::BoardConfig::SuitClass_t;
             using Class_t    = config::BoardConfig::PieceClass_t;
 
+            /**
+             * \brief
+             * The chesspp::board::Board in which the Piece exists.
+             */
             board::Board &board;
         private:
             Position_t p;
@@ -32,15 +40,51 @@ namespace chesspp
             Class_t c;
             std::size_t m = 0;
         public:
+            /**
+             * \brief
+             * The position of this piece.
+             */
             Position_t  const &pos    = p;
+            /**
+             * \brief
+             * The display suit of this piece (e.g. Black or White).
+             */
             Suit_t      const &suit   = s;
+            /**
+             * \brief
+             * The display class of this piece (e.g. Pawn, Rook, etc).
+             * 
+             * \note
+             * This cannot be used to determine what kind of piece this is, as this value is
+             * only for display purposes and may not match the actual type.
+             */
             Class_t     const &pclass = c;
+            /**
+             * \brief
+             * The number of moves this piece has made.
+             */
             std::size_t const &moves  = m;
 
+            /**
+             * \brief
+             * Construct onto a board at the given position with the given suit and class.
+             * 
+             * \param b The chesspp::board::Board on which this piece is to live, must outlive
+             * this instance.
+             * \param pos The initial position of this piece.
+             * \param s The display suit of the piece.
+             * \param pc The display class of the piece.
+             */
             Piece(board::Board &b, Position_t const &pos, Suit_t const &s, Class_t const &pc);
             virtual ~Piece() = default;
 
-            //non-virtual, calls calcTrajectory(), which should call addTrajectory() for each possible tile
+            /**
+             * \brief
+             * Update movement information.
+             * 
+             * \note
+             * Should not be overridden or shadowed by deriving classes.
+             */
             void makeTrajectory()
             {
                 addCapturable(pos);
@@ -48,32 +92,79 @@ namespace chesspp
             }
 
         protected:
-            //should call addTrajectory() for each calculated trajectory
-            //and addCapture() for each possible capture
+            /**
+             * \brief
+             * Called by `makeTrajectory()` to calculate movement information.
+             * 
+             * Implementations should call `addTrajectory()`, `addCapturing()`, and
+             * `addCapturable()`, as well as their `remove` counterparts if necessary. By
+             * default, `addCapturable()` is called with the current position of the piece
+             * before this member function is called.
+             */
             virtual void calcTrajectory() = 0;
 
-            //deriving classes should call this from makeTrajectory to add a calculated trajectory tile
+            /**
+             * \brief
+             * Mark a position as a valid position to move to without capturing.
+             * 
+             * \param tile The position to mark.
+             */
             void addTrajectory(Position_t const &tile);
-            //further deriving classes can call this to remove a trajectory calculated by their parent class
+            /**
+             * \brief
+             * Unmark a position as a valid position to move to without capturing.
+             * 
+             * \param tile The position to unmark.
+             */
             void removeTrajectory(Position_t const &tile);
 
-            //deriving classes should call this from makeTrajectory to add a calculated capturable tile
+            /**
+             * \brief
+             * Mark a position as a valid position to move to when capturing.
+             * 
+             * \param tile The position to mark.
+             */
             void addCapturing(Position_t const &tile);
-            //further deriving classes can call this to remove a capturable tile calculated by their parent class
+            /**
+             * \brief
+             * Unmark a position as a valid position to move to when capturing.
+             * 
+             * \param tile The position to unmark.
+             */
             void removeCapturing(Position_t const &tile);
 
-            //deriving classes should call this from makeTrajectory to add a calculated capturable tile
+            /**
+             * \brief
+             * Mark a position as a valid position to capture this piece from.
+             * 
+             * \param tile The position to mark.
+             */
             void addCapturable(Position_t const &tile);
-            //further deriving classes can call this to remove a capturable tile calculated by their parent class
+            /**
+             * \brief
+             * Unmark a position as a valid position to capture this piece from.
+             * 
+             * \param tile The position to mark.
+             */
             void removeCapturable(Position_t const &tile);
 
         private:
-            //Called with the position of the piece that just moved
+            /**
+             * \brief
+             * Called when another piece moves on the same board.
+             * 
+             * \param m The position of the piece that moved.
+             */
             virtual void tick(Position_t const &m)
             {
             }
 
-            //Sets the piece position as instructed by the board
+            /**
+             * \brief
+             * The chesspp::board::Board calls this to change the position of the piece.
+             * 
+             * \param to The new position of the piece.
+             */
             void move(Position_t const &to)
             {
                 Position_t from = pos;
@@ -82,12 +173,28 @@ namespace chesspp
                 ++m;
             }
 
-            //Called by move(), reacts to being moved
+            /**
+             * \brief
+             * Called after this piece has moved.
+             * 
+             * \note
+             * The value of `moves` is not updated until after this member function returns.
+             * 
+             * \param from The old position of the piece.
+             * \param to The current position of the piece.
+             */
             virtual void moveUpdate(Position_t const &from, Position_t const &to)
             {
             }
 
         public:
+            /**
+             * \brief
+             * Outputs a basic human-readable description of this piece to an output stream.
+             * 
+             * \param os The stream to which to describe this piece.
+             * \param p The piece to describe.
+             */
             friend std::ostream &operator<<(std::ostream &os, Piece const &p)
             {
                 return os << "Piece (" << typeid(p).name() << ") \"" << p.suit << "\" \"" << p.pclass << "\" at " << p.pos << " having made " << p.moves << " moves";

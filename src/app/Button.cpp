98da4189ec04fb38771bdddf4e3764def7d24a8f
Jared Ready <jared.ready@cerner.com>
2015-07-06 12:45:39 -0500

    Fix formatting issues

--- src/app/Button.cpp
+++ src/app/Button.cpp
@@ -2,19 +2,20 @@
 
 namespace chesspp
 {
-  namespace app
-  {
-    Button::Button(const std::string &text, unsigned font_size)
+    namespace app
     {
-      button.setCharacterSize(font_size);
-      button.setString(text);
-      button.setStyle(sf::Text::Bold);
-      button.setColor(sf::Color::Black);
+        Button::Button(const std::string &text, unsigned font_size)
+        {
+            button.setCharacterSize(font_size);
+            button.setString(text);
+            button.setStyle(sf::Text::Bold);
+            button.setColor(sf::Color::Black);
             recenter();
-    }
-    void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
-    {
-        target.draw(button, states);
-    }
+        }
+
+        void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
+        {
+            target.draw(button, states);
+        }
     }
 }
